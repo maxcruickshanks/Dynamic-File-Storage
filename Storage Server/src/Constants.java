a2b1b19d4f09718e12cd8e4cff4b5982e546953f
Maxwell Cruickshanks <maxc7027@icloud.com>
2023-01-04 21:20:31 -0500

    Update Constants.java

--- Storage Server/src/Constants.java
+++ Storage Server/src/Constants.java
@@ -1,8 +1,8 @@
 
 public class Constants {
 	public static final String AES_KEY = "PUT RANDOM AES KEY HERE",
-								FILE_LOCATION = "C:\\LOCATION\\FILE.txt",
-								LOGS_FOLDER = "C:\\LOGS\\";
+				   FILE_LOCATION = "C:\\LOCATION\\FILE.txt",
+				   LOGS_FOLDER = "C:\\LOGS\\";
 	public static final int MAX_LINES = 100000,
-							HOST_PORT = 5000;
+				HOST_PORT = 5000;
 }
